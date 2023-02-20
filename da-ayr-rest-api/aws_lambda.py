31083b309f06e70b27e8bc1509d864e96bbaa1bb
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2023-02-20 14:31:08 +0000

    Allow empty OpenSearch index

--- da-ayr-rest-api/aws_lambda.py
+++ da-ayr-rest-api/aws_lambda.py
@@ -5,9 +5,14 @@ import boto3
 
 ENV_DO_NOT_VERIFY_SSL = 'DO_NOT_VERIFY_SSL'  # e.g. set to TRUE for local testing via ssh tunnel
 ENV_OPENSEARCH_HOST_URL = 'OPENSEARCH_HOST_URL'
+ENV_OPENSEARCH_INDEX = 'OPENSEARCH_INDEX'  # Can set here or in POST payload
 ENV_OPENSEARCH_USER = 'OPENSEARCH_USER'
 ENV_OPENSEARCH_USER_PASSWORD = 'OPENSEARCH_USER_PASSWORD'  # local testing only
 ENV_OPENSEARCH_USER_PASSWORD_PARAM_STORE_KEY = 'OPENSEARCH_USER_PASSWORD_PARAM_STORE_KEY'
+ENV_OPENSEARCH_DEFAULT_INDEX = 'OPENSEARCH_DEFAULT_INDEX'
+JSON_KEY_BODY = 'body'
+JSON_KEY_OPENSEARCH_INDEX = 'opensearch_index'
+JSON_KEY_SOURCE_ORG = 'Source-Organization'
 
 
 def check_verify_ssl_cert() -> bool:
@@ -89,6 +94,25 @@ def get_opensearch_query(source_organization: str) -> str:
     )
 
 
+def get_opensearch_index(event) -> str:
+    """
+    Return the OpenSearch index to use; uses value in environment variable
+    OPENSEARCH_INDEX if present, otherwise value from event payload, or an
+    empty string if no value is set.
+    :param event: AWS Lambda event
+    :return: OpenSearch index name to be used; can be empty.
+    """
+    try:
+        return os.environ[ENV_OPENSEARCH_INDEX]
+    except KeyError:
+        print(f'get_opensearch_index: env var "{ENV_OPENSEARCH_DEFAULT_INDEX}" not set')
+        if JSON_KEY_OPENSEARCH_INDEX in event:
+            print(f'get_opensearch_index: found key {JSON_KEY_OPENSEARCH_INDEX}')
+            return event[JSON_KEY_OPENSEARCH_INDEX]
+        print(f'get_opensearch_index: no index given')
+        return ''
+
+
 def lambda_handler(event, context):
     """
     Handler to run a query against an OpenSearch host.
@@ -104,11 +128,15 @@ def lambda_handler(event, context):
     opensearch_user = os.environ[ENV_OPENSEARCH_USER]
     opensearch_user_password = get_opensearch_user_password()
     credentials = (opensearch_user, opensearch_user_password)
-    opensearch_index = json.loads(event['body'])['opensearch_index']
+    opensearch_index = get_opensearch_index(event)
     print(f'opensearch_index={opensearch_index}')
-    url = opensearch_host_url + f'{opensearch_index}/_search?pretty=true'
+    if len(opensearch_index) > 0:
+        opensearch_index += '/'
+    url = opensearch_host_url + f'{opensearch_index}_search?pretty=true'
     print(f'url={url}')
-    source_organization = json.loads(event['body'])['Source-Organization']
+    source_organization = ''
+    if JSON_KEY_SOURCE_ORG in event:
+        source_organization = event[JSON_KEY_SOURCE_ORG]
     print(f'source_organization={source_organization}')
     opensearch_query_json = get_opensearch_query(source_organization)
 
