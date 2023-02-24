0f5b1e2729f8bf6ee7276aafc6c3af6f4b71c012
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2023-02-24 15:55:32 +0000

    Implemented da-ayr-bag-unpacker lambda

--- da-ayr-bag-unpacker/lambda_function.py
+++ da-ayr-bag-unpacker/lambda_function.py
@@ -1,7 +1,18 @@
+import os
+import tar_lib
+
+
 class AYRBagUnpackerError(Exception):
     """
     For Lambda errors.
     """
+
+
+S3_OUTPUT_PREFIX = os.getenv('AYR_S3_OUTPUT_PREFIX', default='ayr-in/')
+PATH_JOIN = '/'
+KEY_S3_BUCKET = 's3_bucket'
+KEY_BAG_NAME = 'bag_name'
+KEY_BAG_OUTPUT_S3_PATH = 'bag_output_s3_path'
 
 
 def lambda_handler(event, context):
@@ -20,9 +31,32 @@ def lambda_handler(event, context):
     :param context: AWS Lambda context
     :return: AWS Lambda response
     """
+    print(f'event:\n{event}')
+    print(f'context:\n{context}')
+
+    if KEY_S3_BUCKET not in event:
+        raise AYRBagUnpackerError(f'Key "{KEY_S3_BUCKET} not found"')
+    if KEY_BAG_NAME not in event:
+        raise AYRBagUnpackerError(f'Key "{KEY_BAG_NAME} not found"')
+    if KEY_BAG_OUTPUT_S3_PATH not in event:
+        raise AYRBagUnpackerError(f'Key "{KEY_BAG_OUTPUT_S3_PATH} not found"')
+
+    s3_bucket = event[KEY_S3_BUCKET]
+    bag_name = event[KEY_BAG_NAME]
+    bag_output_s3_path = event[KEY_BAG_OUTPUT_S3_PATH]
+
+    extracted_object_names = tar_lib.untar_s3_object(
+        input_bucket_name=s3_bucket,
+        output_bucket_name=s3_bucket,
+        object_name=bag_output_s3_path,
+        output_prefix=S3_OUTPUT_PREFIX + bag_name + PATH_JOIN
+    )
+
     response = {
-        'statusCode': 200,
-        'body': {}
+        's3_bucket': s3_bucket,
+        'bag_name': bag_name,
+        'bag_output_s3_path': bag_output_s3_path,
+        'unpacked_files': extracted_object_names
     }
 
     return response
